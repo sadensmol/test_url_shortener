c62feca2ee781065df56d5b2d75daccc0c5793a7
sadensmol <sadensmol@gmail.com>
2019-07-29 10:26:25 +0300

    last version with pack of tests and final redirect logic, simple errors handling

--- src/test/java/me/sadensmol/urlshortener/TestUrlController.java
+++ src/test/java/me/sadensmol/urlshortener/TestUrlController.java
@@ -8,6 +8,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,6 +37,10 @@ public class TestUrlController {
     @Autowired
     private UrlController urlController;
 
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
     @Test
     public void diTest()    {
         assertThat(urlController).isNotNull();
@@ -34,17 +48,22 @@ public class TestUrlController {
     }
 
     @Test
-    public void test() throws MalformedURLException {
+    public void test() throws Exception {
         String url = "http://www.ya.ru";
         String shortUrl = this.restTemplate.getForObject("http://localhost:"+port+"/shorten?url={url}", String.class, url);
         assertThat(shortUrl).isNotEmpty();
 
         URL testUrl = new URL(shortUrl);
 
-        String fullUrl = this.restTemplate.getForObject("http://localhost:"+port+"/url?shortUrl={shortUrl}", String.class, shortUrl);
-        assertThat(fullUrl).isNotEmpty();
-        assertThat(fullUrl).isEqualTo("redirect:redirect?url="+url);
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
 
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.get("http://localhost:"+port+"/url?shortUrl="+shortUrl);
+
+        this.mockMvc.perform(builder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.redirectedUrl
+                        (url));
     }
 
     //todo add unique test/ wrong url test etc...
